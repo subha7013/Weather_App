938ebef3e22f84feb9a33e4cc4f60c5a1b086cea
Subhasish Nath <nathsubhasish55@gmail.com>
2025-10-15 10:58:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,48 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, session
 import requests
 
 app = Flask(__name__)
 
-# Get API key from environment variable
-API_KEY = os.getenv("API_KEY")
+API_KEY = "d95882f9eadd331b022755a55eed0a95"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     message = ""
+    
     if request.method == "POST":
         city = request.form.get("city")
+
         if city:
             params = {
                 "q": city,
                 "appid": API_KEY,
                 "units": "metric"
             }
+
+            response = requests.get(BASE_URL, params=params)
+
             try:
-                response = requests.get(BASE_URL, params=params)
                 if response.status_code == 200:
                     data = response.json()
                     message = (
                         f"Weather in {city} 🌍\n"
-                        f"🌡️  Temperature : {data['main']['temp']} °C\n"
-                        f"🌨️  Condition : {data['weather'][0]['description'].capitalize()}\n"
-                        f"☁  Humidity : {data['main']['humidity']} %\n"
-                        f"💨  Wind Speed : {data['wind']['speed']} m/s\n"
+                        f"🌡️ Temperature : {data['main']['temp']} °C\n"
+                        f"🌨️ Condition : {data['weather'][0]['description'].capitalize()}\n"
+                        f"☁ Humidity : {data['main']['humidity']} %\n"
+                        f"💨 Wind Speed : {data['wind']['speed']} m/s\n"
                     )
                 else:
                     message = "❌ City not found or error in fetching data..."
             except Exception as e:
                 message = f"⚠️ Error: {str(e)}"
         else:
-            message = "Please enter a city name..."
+            print("Please enter a city name...")
 
     return render_template("index.html", message=message)
 
+
 if __name__ == "__main__":
-    print("🚀 Starting Flask server locally...")
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    print("🚀 Starting Flask server...")
+    app.run(debug=True)
